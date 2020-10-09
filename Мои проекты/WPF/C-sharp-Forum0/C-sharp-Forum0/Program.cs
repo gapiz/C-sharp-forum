6bb99753a00574290afb368944a9bbd69a4d3be8
Gapiz <gapizmurtuz@gmail.com>
2020-10-09 23:04:22 +0300

    Первый урок 01

---
+++ Мои проекты/WPF/C-sharp-Forum0/C-sharp-Forum0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Forum0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свой год рождения: ");
+            int dateOfBirth = int.Parse(Console.ReadLine());
+            Console.Write("Введите текущий год: ");
+            int nowYear = int.Parse(Console.ReadLine());
+            int age = nowYear - dateOfBirth;
+            Console.Write("Ваш возраст: "+age);
+            Console.ReadKey();
+        }
+    }
+}
